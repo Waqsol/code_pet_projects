e05ce80c62a30d50177e25b77a1f6097adbad530
Sergey <Pulpu070602@yandex.ru>
2024-04-10 10:38:30 +0800

    update
    hide secret key

--- Portal. Python + Django/app_project/app_project/settings.py
+++ Portal. Python + Django/app_project/app_project/settings.py
@@ -1,6 +1,9 @@
 import os
 import sys
 from pathlib import Path
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,7 +14,6 @@ LOG_DIR = os.path.join(SETTINGS_DIR, 'logs')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%p15fzltk^^049_4a&r_8n$74(jmjy6rk9wl1q-7odi2^@nrfn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
