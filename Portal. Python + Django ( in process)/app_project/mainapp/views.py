c603adbf5a36dbb1ead3a694202404e42ac16ebb
Sergey <85299887+Waqsol@users.noreply.github.com>
2024-04-10 09:51:34 +0800

    Delete Portal. Python + Django ( in process) directory

--- Portal. Python + Django ( in process)/app_project/mainapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'mainapp/index.html')
-
-
-def about(request):
-    return render(request, 'mainapp/about.html')
